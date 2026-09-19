8c0af68336fa40fd5b5f0eda45ac8bfd0ddae0ec
Avishek Thapa <avishekthapa1997@gmail.com>
2026-09-19 23:29:24 +0530

    Use try catch for handling the dbcall

--- src/jobs/processor/JobProcessor.ts
+++ src/jobs/processor/JobProcessor.ts
@@ -3,7 +3,6 @@ import { APP_CONSTANTS } from '../../common/constants/app-constants.js';
 import { Job } from 'bullmq';
 import { EmailPayload } from '../payload/email.payload.js';
 import { PrismaService } from '../../db/db.service.js';
-import { EmailService } from '../../email/email.service.js';
 import { Logger } from '@nestjs/common';
 import { formatJobId } from '../util/index.js';
 import { JobWorkerFactory } from '../worker/worker.factory.js';
@@ -18,45 +17,62 @@ export class JobProcessor extends WorkerHost {
   ) {
     super();
   }
-  async process(job: Job): Promise<any> {
-    let jobId = job.id;
-    if (!jobId) {
-      return;
-    }
-    const dbjobId = formatJobId(jobId);
-    const dbJob = await this.prismaService.job.findUnique({
-      where: {
-        id: dbjobId,
-      },
-    });
-    if (!dbJob) {
-      return;
-    }
-    if (dbJob.status === 'COMPLETED') {
-      return;
-    }
-    const worker = this.workerFactory.getWorker(dbJob.type as JobType);
-    await worker.execute(job);
-  }
 
-  @OnWorkerEvent('active')
-  async onActive(job: Job) {
+  async process(job: Job): Promise<any> {
     try {
       if (!job.id) {
         return;
       }
-      const dbJobId = formatJobId(job.id);
-      await this.prismaService.job.update({
+      const dbjobId = formatJobId(job.id);
+      const dbJob = await this.prismaService.job.findUnique({
         where: {
-          id: dbJobId,
-          OR: [{ status: 'PENDING' }, { status: 'FAILED' }],
+          id: dbjobId,
+        },
+      });
+      if (!dbJob) {
+        return;
+      }
+      if (dbJob.status === 'COMPLETED') {
+        return;
+      }
+      const worker = this.workerFactory.getWorker(dbJob.type as JobType);
+      await worker.execute(job);
+    } catch (err) {
+      this.logger.error(`Failed to process job ${job.id}`, err);
+      throw err;
+    }
+  }
+
+  private isJobRetryAttempLeft(job: Job) {
+    const maxAttempt = job.opts.attempts;
+    const attemptsMade = job.attemptsMade;
+    if (maxAttempt !== undefined && maxAttempt > 0) {
+      return attemptsMade < maxAttempt;
+    }
+    return false;
+  }
+
+  @OnWorkerEvent('active')
+  async onActive(job: Job) {
+    if (!job.id) {
+      return;
+    }
+    const jobId = formatJobId(job.id);
+    try {
+      const result = await this.prismaService.job.updateMany({
+        where: {
+          id: jobId,
+          status: 'PENDING',
         },
         data: {
           status: 'PROCESSING',
         },
       });
+      if (result.count > 0) {
+        this.logger.log(`${job.id} started processing`);
+      }
     } catch (err) {
-      this.logger.error('Failed to mark job as processing :', err);
+      this.logger.error(`Failed to mark job: ${jobId} as processing`);
     }
   }
 
@@ -66,15 +82,22 @@ export class JobProcessor extends WorkerHost {
       throw new Error('Job is not available');
     }
     const jobId = formatJobId(job.id);
-    await this.prismaService.job.update({
-      where: {
-        id: jobId,
-        status: 'PROCESSING',
-      },
-      data: {
-        status: 'COMPLETED',
-      },
-    });
+    try {
+      const result = await this.prismaService.job.updateMany({
+        where: {
+          id: jobId,
+          status: 'PROCESSING',
+        },
+        data: {
+          status: 'COMPLETED',
+        },
+      });
+      if (result.count > 0) {
+        this.logger.log(`Job: ${job.id} is completed.`);
+      }
+    } catch (err) {
+      this.logger.log(`Failed to mark job ${jobId} as completed.`);
+    }
   }
 
   @OnWorkerEvent('stalled')
@@ -88,32 +111,30 @@ export class JobProcessor extends WorkerHost {
     error: Error,
     prev: string,
   ) {
-    if (!job) {
+    if (!job || !job.id) {
       return;
     }
-    let jobId = job.id;
-    if (!jobId) {
-      return;
+    const jobId = formatJobId(job.id);
+    try {
+      const retryAttemptLeft = this.isJobRetryAttempLeft(job);
+      if (retryAttemptLeft) {
+        return;
+      }
+      this.logger.error('Job: ', jobId, ' failed', error, prev);
+      const result = await this.prismaService.job.updateMany({
+        where: {
+          id: jobId,
+          status: 'PROCESSING',
+        },
+        data: {
+          status: 'FAILED',
+        },
+      });
+      if (result.count > 0) {
+        this.logger.log(`Job ${job.id} failed`, error);
+      }
+    } catch (err) {
+      this.logger.error(`Failed to mark job: ${jobId} as failed`);
     }
-    const dbJobId = formatJobId(jobId);
-    const maxAttempt = job.opts.attempts;
-    const attemptsMade = job.attemptsMade;
-    let retryAttemptLeft = false;
-    if (maxAttempt !== undefined && maxAttempt > 0) {
-      retryAttemptLeft = attemptsMade < maxAttempt;
-    }
-    if (retryAttemptLeft) {
-      return;
-    }
-    this.logger.error('Job: ', jobId, ' failed', error, prev);
-    await this.prismaService.job.update({
-      where: {
-        id: dbJobId,
-        status: 'PROCESSING',
-      },
-      data: {
-        status: 'FAILED',
-      },
-    });
   }
 }
